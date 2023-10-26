3eedbc3ae4198fc3739363ffb8ef152230616c60
SOOS-JAlvarez <jalvarez@soos.io>
2023-10-26 11:37:51 -0300

    change file format to csa.json cleanup constants

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,11 +1,5 @@
 export const FILE_ENCODING = "utf8";
-export const JSON_CONTENT_TYPE = "application/json";
-export const MAX_MANIFESTS = 50;
-export const KILO_BYTE = 1024;
-export const SOOS_PROJECTS_API = "api-projects.soos.io";
-export const SOOS_ANALYSIS_API = "api.soos.io";
-export const DEFAULT_SCAN_FORMAT = "json";
-export const DEFAULT_FILE_PATH = "./results/results.json";
+export const DEFAULT_FILE_PATH = "./results/results.csa.json";
 export const DEFAULT_COMMAND_TEMPLATE = "syft {image} -o json={file_path}";
 export const SOOS_API_KEY_ENV_VAR = "SOOS_API_KEY";
 export const SOOS_CLIENT_ID_ENV_VAR = "SOOS_CLIENT_ID";
