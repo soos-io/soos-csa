c147bbde8a9ffe8bcc786e28b0568e16c7bd452a
SOOS-JAlvarez <92373106+SOOS-JAlvarez@users.noreply.github.com>
2023-11-07 11:06:08 -0300

    PA-11719 Link to project url instead of status (#8)

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,5 +1,12 @@
-export const FILE_ENCODING = "utf8";
-export const DEFAULT_FILE_PATH = "./results/results.csa.json";
-export const DEFAULT_COMMAND_TEMPLATE = "syft {image} -o json={file_path}";
-export const SOOS_API_KEY_ENV_VAR = "SOOS_API_KEY";
-export const SOOS_CLIENT_ID_ENV_VAR = "SOOS_CLIENT_ID";
+export const CONSTANTS = {
+  FILES: {
+    DEFAULT_FILE_PATH: "./results/results.csa.json",
+  },
+  SOOS: {
+    API_KEY_ENV_VAR: "SOOS_API_KEY",
+    CLIENT_ID_ENV_VAR: "SOOS_CLIENT_ID",
+  },
+  CSA: {
+    TOOL_NAME: "syft",
+  },
+};
