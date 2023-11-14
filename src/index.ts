0d9f55761456d9d6c889c092a1901ff26365d89b
SOOS-JAlvarez <jalvarez@soos.io>
2023-11-14 11:19:24 -0300

    added integration type and name defaults

--- src/index.ts
+++ src/index.ts
@@ -97,11 +97,13 @@ class SOOSCSAAnalysis {
     parser.add_argument("--integrationName", {
       help: "Integration Name - Intended for internal use only.",
       required: false,
+      default: CONSTANTS.SOOS.DEFAULT_INTEGRATION_NAME,
     });
 
     parser.add_argument("--integrationType", {
       help: "Integration Type - Intended for internal use only.",
       required: false,
+      default: CONSTANTS.SOOS.DEFAULT_INTEGRATION_TYPE,
     });
 
     parser.add_argument("--logLevel", {
